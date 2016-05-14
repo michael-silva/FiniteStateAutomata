80bad022e14c85c87521bce2e54211191839e9e3
Michael Silva <michaelpereiradasilva@hotmail.com>
2016-05-13 21:03:52 -0300

    Rename classes and folders to easier the use

--- src/FiniteStateAutomata/Automata/Interfaces/IAutomataAlphabet.cs
+++
@@ -1,9 +0,0 @@
-namespace FiniteStateAutomata.Automata.Interfaces
-{
-    public interface IAutomataAlphabet<T>
-    {
-        int Count { get; }
-        
-        int IndexByValue(T value);
-    }
-}
